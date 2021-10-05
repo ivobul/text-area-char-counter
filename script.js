4ec1c24c5d3babc7ea19e5e63df037971b47e6ce
ivobul <24499940+ivobul@users.noreply.github.com>
2021-10-05 08:10:20 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,21 +3,23 @@ const output = document.querySelector("#output");
 let counter = 60;
 
 textArea.addEventListener("keydown", (e) => {
-  textAreaCharCounter(e);
+  displayCharCounter(e);
 });
 
-function textAreaCharCounter(e) {
-  const keycode = e.keyCode;
+function isPrintableChar(e) {
   // Determine if e.keyCode is a printable (non-control) character
   // Original source: https://stackoverflow.com/a/12467610
-  const valid = (keycode > 47 && keycode < 58)   || // number keys
-        keycode == 32 || keycode == 13 || keycode == 60   || // spacebar, return key(s) (if you want to allow carriage returns), and <
-        (keycode > 64 && keycode < 91)   || // letter keys
-        (keycode > 95 && keycode < 112)  || // numpad keys
-        (keycode > 185 && keycode < 193) || // ;=,-./` (in order)
-        (keycode > 218 && keycode < 223);   // [\]' (in order)
-  
-  if (valid) {
+  const valid = (e.keyCode > 47 && e.keyCode < 58)   || // number keys
+        e.keyCode == 32 || e.keyCode == 13 || e.keyCode == 60   || // spacebar, return key(s) (if you want to allow carriage returns), and <
+        (e.keyCode > 64 && e.keyCode < 91)   || // letter keys
+        (e.keyCode > 95 && e.keyCode < 112)  || // numpad keys
+        (e.keyCode > 185 && e.keyCode < 193) || // ;=,-./` (in order)
+        (e.keyCode > 218 && e.keyCode < 223);   // [\]' (in order)
+  return valid;
+}
+
+function displayCharCounter(e) {
+  if (isPrintableChar(e)) {
     if (counter > 0) {
       counter--;
       output.innerText = `${counter} characters left`;
@@ -27,7 +29,7 @@ function textAreaCharCounter(e) {
     }
   }
 
-  else if (keycode === 8) {
+  else if (e.keyCode === 8) {
     if (counter < 60) {
       counter++;
       output.innerText = `${counter} characters left`;
